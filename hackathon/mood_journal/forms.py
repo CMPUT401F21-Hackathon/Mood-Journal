eb1f75dd7fb57af3dd212df85c4b60d8052f9169
unknown <tlnguyen@ualberta.ca>
2021-09-11 15:47:32 -0600

    Update mood new status

--- hackathon/mood_journal/forms.py
+++ hackathon/mood_journal/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Mood
 
 class MoodForm(forms.ModelForm):
-    
+    status = forms.CharField(initial="Untitled Mood")
     class Meta:
         model = Mood
         fields = ['status', 'recommendation',]
